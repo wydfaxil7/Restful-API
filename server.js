5ee0e80da843f1e6c811d91ba12dd3d12ee56ad6
Ahmed Fazil <144231837+wydfaxil7@users.noreply.github.com>
2024-05-18 05:02:19 +0500

    Applying operations on mongoDB, CREATE MODEL

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Product = require('./models/productModels')
 const app = express()
+
+// using the express middleware to let us interact with the Database
+app.use(express.json());
 
 //to access the port on browser, we need to make routes
 // also we can make multiple routes
@@ -14,7 +18,16 @@ app.get('/blog', (req, res) => {
     //nodemon working perfectly
 })
 
-
+// now here is the procedure on how we can use the product model
+app.post('/product', async(req, res) => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message});
+    }
+})
 
 mongoose.connect('mongodb+srv://wydfaxil07:Aprinov-1428@wydfaxilapi.9k2ck6z.mongodb.net/NODE-Restful-API')
 .then(() => {
